92e0b73a6f768606ab1c4cc1c78d1392f62ea0f2
Afshin Izadnia <119739877+izadnia@users.noreply.github.com>
2022-12-19 09:26:05 +0330

    Coordinate container
    The coordinate container of all the bombs is created

--- App/diffuser.py
+++ App/diffuser.py
@@ -4,3 +4,8 @@ def creat_board():
     X = int(input("Please specify the length of the page by entering an integer:"))
     Y = int(input("Please Specify the width of the page by entering an integer:"))
     B_count = int(input("Determine the number of bombs: "))
+
+    
+    Bomb_list = []
+    
+    
